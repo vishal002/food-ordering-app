94307e70436ec49a2fcbdd832a0c228a820ddaef
vishal kumar <vkumar61@slb.com>
2021-08-30 12:41:16 +0530

    Working with Refs & Forward Refs

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,36 @@
+import { useReducer } from "react";
 import CartContext from "./cart-contect";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
-  const cartContext = {
+const defaultCartState = {
     items: [],
-    totalAmount: 0,
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+    return defaultCartState
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type: 'ADD', item: item});
+  };
+  const removeItemFromCartHandler = (id) => {
+      dispatchCartAction({type: 'REMOVE', id: id});
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
